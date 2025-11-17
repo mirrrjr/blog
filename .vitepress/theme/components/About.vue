956aaa662db933612e4ed61abbf17de7e2f74c66
mirrrjr <mirrrrrr@yahoo.com>
2025-11-17 12:40:13 +0500

    chamge about page

--- .vitepress/theme/components/About.vue
+++ .vitepress/theme/components/About.vue
@@ -1,84 +1,88 @@
 <template>
-  <div class="container">
+  <div class="container-box">
     <p align="center">
-      <img src="https://iili.io/dBau4ZN.gif)](https://freeimage.host/i/dBau4ZN" alt="banner" />
+      <img
+        src="https://iili.io/dBau4ZN.gif)](https://freeimage.host/i/dBau4ZN"
+        alt="banner"
+      />
     </p>
 
     <p align="center">
-      <samp class="links">
-        <a href="https://mirrr.uz">blog</a>
-        <span>|</span>
-        <a href="https://www.linkedin.com/in/mirrrjr/">linkedin</a>
-        <span>|</span>
+      <samp>
+        <a href="https://blog.mirrr.uz/">blog</a> .
+        <a href="https://mirrr.uz/">portfolio</a> .
+        <a href="https://www.linkedin.com/in/mirrrjr/">linkedin</a> .
+        <a href="https://bsky.app/profile/mirrr.uz">bsky</a> .
         <a href="https://t.me/mirrrjr">telegram</a>
       </samp>
     </p>
 
-    <div class="table-wrapper">
-      <table>
-        <thead>
-          <tr>
-            <th>Language</th>
-            <th>Backend</th>
-            <th>Frontend</th>
-            <th>DataBase</th>
-            <th>Extra</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td align="center">
-              <img class="table_img" src="https://skillicons.dev/icons?i=php" />
-              <img class="table_img" src="https://skillicons.dev/icons?i=typescript" />
-              <img class="table_img" src="https://skillicons.dev/icons?i=rust" />
-            </td>
-            <td align="center">
-              <img class="table_img" src="https://skillicons.dev/icons?i=laravel" />
-              <img class="table_img" src="https://skillicons.dev/icons?i=nestjs" />
-              <img class="table_img" src="https://skillicons.dev/icons?i=actix" />
-            </td>
-            <td align="center">
-              <img class="table_img" src="https://skillicons.dev/icons?i=react" />
-              <img class="table_img" src="https://skillicons.dev/icons?i=angular" />
-              <img class="table_img" src="https://skillicons.dev/icons?i=vue" />
-            </td>
-
-            <td align="center">
-              <img class="table_img" src="https://skillicons.dev/icons?i=mysql" />
-              <img class="table_img" src="https://skillicons.dev/icons?i=postgresql" />
-              <img class="table_img" src="https://skillicons.dev/icons?i=mongodb" />
-            </td>
-            <td align="center">
-              <img class="table_img" src="https://skillicons.dev/icons?i=linux" />
-              <img class="table_img" src="https://skillicons.dev/icons?i=git" />
-              <img class="table_img" src="https://skillicons.dev/icons?i=docker" />
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <table align="center">
+      <thead>
+        <tr>
+          <th>Language</th>
+          <th>Backend</th>
+          <th>DataBase</th>
+          <th>Extra</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td align="center">
+            <img src="https://skillicons.dev/icons?i=typescript" />
+            <img src="https://skillicons.dev/icons?i=php" />
+            <img src="https://skillicons.dev/icons?i=rust" />
+          </td>
+          <td align="center">
+            <img src="https://skillicons.dev/icons?i=laravel" />
+            <img src="https://skillicons.dev/icons?i=nestjs" />
+            <img src="https://skillicons.dev/icons?i=actix" />
+          </td>
+          <td align="center">
+            <img src="https://skillicons.dev/icons?i=mongodb" />
+            <img src="https://skillicons.dev/icons?i=postgresql" />
+            <img src="https://skillicons.dev/icons?i=mysql" />
+          </td>
+          <td align="center">
+            <img src="https://skillicons.dev/icons?i=linux" />
+            <img src="https://skillicons.dev/icons?i=git" />
+            <img src="https://skillicons.dev/icons?i=nix" />
+          </td>
+        </tr>
+      </tbody>
+    </table>
 
     <p align="center">
       <a href="https://hits.sh/github.com/mirrrjr/">
         <img
-          src="https://hits.sh/github.com/mirrrjr.svg?view=today-total&label=mirrr's%20viewers%20(today%20%2F%20total)&color=ff5a00&labelColor=0a0505">
+          src="https://hits.sh/github.com/mirrrjr.svg?view=today-total&label=mirrr's%20viewers%20(today%20%2F%20total)&color=ff5a00&labelColor=0a0505"
+        />
       </a>
     </p>
 
     <p align="center">
-      <img src="https://iili.io/dBau4ZN.gif)](https://freeimage.host/i/dBau4ZN" alt="banner" />
+      <img
+        src="https://iili.io/dBau4ZN.gif)](https://freeimage.host/i/dBau4ZN"
+        alt="banner"
+      />
     </p>
   </div>
 </template>
 
 <style>
+.container-box {
+  display: flex !important;
+  flex-direction: column !important;
+  align-items: center !important;
+}
+
 .links {
   display: flex;
   justify-content: center;
   align-items: center;
 }
 
-.links a{
+.links a {
   margin-right: 5px;
   margin-left: 5px;
 }
